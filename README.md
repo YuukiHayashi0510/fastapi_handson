f7bbeaad5933d2db49af49643cc8f7d0be0fc3c6
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-05-21 15:25:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Akarenga-FastAPI
+
+## install
+
+```zsh
+pip install uvicorn[\standard\] fastapi[\all\]
+# or
+pip install -r requirements.txt
+```
+
+## memo
+
+- uvicorn
+  - 読み方例：アバコーン
+  - 非同期処理特化の Web サーバー
+  - ASGI（Asynchronous Server Gateway Interface）と呼ばれる Python の Web サーバと Web アプリケーションが通信するための WSGI（Web Server Gateway Interface）の後継のインターフェース定義
+- pydantic
+  - Python で型定義できるライブラリ
+
+## 個人メモ
+
+- [async def の謎](https://fastapi.tiangolo.com/ja/async/)
+- [sqlite3 のチートシート](https://qiita.com/sotetsuk/items/cd2aeae4ba7e72faad47)
+- `/docs`で FastAPI のドキュメントが自動出力される
--- main.py
+++ main.py
@@ -1,14 +1,67 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from databases import Database
+from model import Task
 
 app = FastAPI()
+database = Database("sqlite:///tasks.db")
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+# データベースに接続する
+@app.on_event("startup")
+async def startup():
+    await database.connect()
 
 
-# url指定、変数受け取る時これ
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):  # ? asyncつく理由
-    return {"item_id": item_id}  # JSON
+# データベースとの接続を遮断する
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.post("/tasks/", response_model=Task)
+async def create_task(task: Task):
+    query = "INSERT INTO tasks (title, description, completed) VALUES (:title, :description, :completed)"
+    values = task.dict()
+    task_id = await database.execute(query=query, values=values)
+    response = {"id": task_id, **values}
+    return JSONResponse(content=response, status_code=201)  # 201=created
+
+
+@app.get("/tasks/", response_model=Task)
+async def read_tasks(skip: int = 0, limit: int = 100):
+    # skip：データを飛ばし飛ばしで取得する, limit：id 1~100まで取得
+    query = "SELECT * FROM tasks ORDER BY id DESC LIMIT :limit OFFSET :skip"
+    tasks = await database.fetch_all(query=query, values={"skip": skip, "limit": limit})
+    return tasks
+
+
+@app.get("/tasks/{task_id}", response_model=Task)  # {変数}
+async def read_task(task_id: int):
+    query = "SELECT * FROM tasks WHERE id = :id"
+    task = await database.fetch_one(query=query, values={"id": task_id})
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+
+@app.put("/tasks/{task_id}", response_model=Task)
+async def update_task(task_id: int, task: Task):
+    query = """
+        UPDATE tasks SET title = :title, description = :description, completed = :completed
+        WHERE id = :id
+    """
+    values = {"id": task_id, **task.dict()}  # **=可変長引数:dict
+    result = await database.execute(query=query, values=values)
+    if not result:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {**values}
+
+
+@app.delete("/tasks/{task_id}")  # 返却しないからresponse_model不要
+async def delete_task(task_id: int):
+    query = "DELETE FROM tasks WHERE id = :id"
+    result = await database.execute(query=query, values={"id": task_id})
+    if not result:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"message": "Task deleted successfully"}
